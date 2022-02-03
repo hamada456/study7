2b8a4e8c4f4a2eca3e57c5df7326567870146cbb
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-02-03 14:15:11 +0900

    表示まで仮完成

--- index.js
+++ index.js
@@ -2,23 +2,20 @@
 
     getJSON();
 
-    //document.getElementsByTagName("th")[0].textContent = employee.employee[0].name
-    //document.getElementsByTagName("th")[1].textContent = employee.employee[0].status
-
     let $table = document.getElementsByClassName('table');
     let newTh = document.createElement("th");
     let newTr = document.createElement("tr");
-    //newTr.textContent = employee.employee[1].name;
 
-    //tr01
+    for(index = 0 ; employee.employee.length > index ; index++ ){
+    //tr001
     $table[0].appendChild(document.createElement("tr"));
-    //th
+    //th1
     document.getElementsByClassName("table")[0].lastElementChild.appendChild(document.createElement("th"));
-    document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[0].name;
-    //th
-    document.getElementsByClassName("table")[0].lastElementChild.appendChild(document.createElement("th"));
-    document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[0].status;
-    
+    document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[index].name;
+    //th2
+    document.querySelectorAll("tr")[document.querySelectorAll("tr").length -1].lastElementChild.after(document.createElement("th"));
+    document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[index].status;
+    }
 
     function getJSON() {
         // XMLHttpRequest オブジェクトを生成する
@@ -29,7 +26,5 @@
         // JSON のデータ数分処理、値を返す
         employee = JSON.parse(req.responseText);
     }
-
-    //window.open("",width=50 ,heigth=50);
 
 })();
