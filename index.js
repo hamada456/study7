1c969fc54025648454f1fa420b13f599749cd60a
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-02-03 16:20:42 +0900

    リファクタリング完了

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 (() => {
 
-    getJSON();
-
+    const $doc = document;
     const $table = document.getElementsByClassName('table');
-    const newTh = document.createElement("th");
-    const newTr = document.createElement("tr");
-    let thElementNum = document.querySelectorAll("th").length -1
-    let trElementNum = document.querySelectorAll("tr").length -1
 
-    for( index = 0 ; employee.employee.length > index ; index++ ){
+    getJSON();
+    
+    //社員の数だけループ
+    for ( index = 0 ; employee.employee.length > index ; index++ ) {
+        let thElementEndNum = document.querySelectorAll("th").length;
+        let trElementEndNum = document.querySelectorAll("tr").length;
+        let newTh = document.createElement("th");
+
         //tr001、大枠を作成
         $table[0].appendChild(document.createElement("tr"));
         //th1、枠を作成
-        document.getElementsByClassName("table")[0].lastElementChild.appendChild(document.createElement("th"));
+        document.getElementsByClassName("table")[0].lastElementChild.appendChild(newTh);
         //名前を代入
-        document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[index].name;
+        document.querySelectorAll("th")[thElementEndNum].textContent = employee.employee[index].name;
         //th2、枠を作成
-        document.querySelectorAll("tr")[document.querySelectorAll("tr").length -1].lastElementChild.after(document.createElement("th"));
+        document.querySelectorAll("tr")[trElementEndNum].lastElementChild.after(document.createElement("th"));
         //状態を代入
-        document.querySelectorAll("th")[document.querySelectorAll("th").length -1].textContent = employee.employee[index].status;
+        document.querySelectorAll("th")[thElementEndNum + 1].textContent = employee.employee[index].status;
     }
 
     function getJSON() {
